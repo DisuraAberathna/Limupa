d175ae801ee7bcd608d688015e587b563145c516
Disura Chathwara <disura2005@gmail.com>
2024-09-09 15:34:39 +0530

    update user verification

--- src/java/controller/UserVerification.java
+++ src/java/controller/UserVerification.java
@@ -34,22 +34,98 @@ public class UserVerification extends HttpServlet {
         Gson gson = new Gson();
         JsonObject reqObject = gson.fromJson(req.getReader(), JsonObject.class);
         String otp = reqObject.get("otp").getAsString();
-        
-        if (req.getSession().getAttribute("email") != null) {
+
+        if (req.getSession().getAttribute("id") != null && req.getSession().getAttribute("email") != null && req.getSession().getAttribute("otp") != null) {
+            String id = req.getSession().getAttribute("id").toString();
             String email = req.getSession().getAttribute("email").toString();
-            
+            String sesotp = req.getSession().getAttribute("otp").toString();
+
+            if (sesotp.equals(otp)) {
+                Session session = HibernateUtil.getSessionFactory().openSession();
+
+                Criteria criteria = session.createCriteria(User.class);
+                criteria.add(Restrictions.eq("id", Integer.valueOf(id)));
+
+                if (!criteria.list().isEmpty()) {
+                    User user = (User) criteria.list().get(0);
+                    user.setEmail(email);
+
+                    session.update(user);
+                    session.beginTransaction().commit();
+
+                    UserDTO userDTO = new UserDTO();
+                    userDTO.setId(user.getId());
+                    userDTO.setF_name(user.getF_name());
+                    userDTO.setL_name(user.getL_name());
+                    userDTO.setEmail(email);
+
+                    req.getSession().removeAttribute("id");
+                    req.getSession().removeAttribute("email");
+                    req.getSession().removeAttribute("otp");
+                    req.getSession().setAttribute("user", userDTO);
+
+                    responseDTO.setOk(true);
+                    responseDTO.setMsg("Your password updated!");
+                } else {
+                    responseDTO.setMsg("Something went wrong!");
+                }
+            } else {
+                responseDTO.setMsg("Invalid verification code!");
+            }
+
+        } else if (req.getSession().getAttribute("id") != null && req.getSession().getAttribute("password") != null && req.getSession().getAttribute("otp") != null) {
+            String id = req.getSession().getAttribute("id").toString();
+            String password = req.getSession().getAttribute("password").toString();
+            String sesotp = req.getSession().getAttribute("otp").toString();
+
+            if (sesotp.equals(otp)) {
+                Session session = HibernateUtil.getSessionFactory().openSession();
+
+                Criteria criteria = session.createCriteria(User.class);
+                criteria.add(Restrictions.eq("id", Integer.valueOf(id)));
+
+                if (!criteria.list().isEmpty()) {
+                    User user = (User) criteria.list().get(0);
+                    user.setPassword(password);
+
+                    session.update(user);
+                    session.beginTransaction().commit();
+
+                    UserDTO userDTO = new UserDTO();
+                    userDTO.setId(user.getId());
+                    userDTO.setF_name(user.getF_name());
+                    userDTO.setL_name(user.getL_name());
+                    userDTO.setEmail(user.getEmail());
+
+                    req.getSession().removeAttribute("id");
+                    req.getSession().removeAttribute("password");
+                    req.getSession().removeAttribute("otp");
+                    req.getSession().setAttribute("user", userDTO);
+
+                    responseDTO.setOk(true);
+                    responseDTO.setMsg("Your password updated!");
+                } else {
+                    responseDTO.setMsg("Something went wrong!");
+                }
+            } else {
+                responseDTO.setMsg("Invalid verification code!");
+            }
+
+        } else if (req.getSession().getAttribute("email") != null) {
+            String email = req.getSession().getAttribute("email").toString();
+
             Session session = HibernateUtil.getSessionFactory().openSession();
             Criteria criteria = session.createCriteria(User.class);
             criteria.add(Restrictions.eq("email", email));
             criteria.add(Restrictions.eq("verification", otp));
-            
+
             if (!criteria.list().isEmpty()) {
                 User user = (User) criteria.list().get(0);
                 user.setVerification("Verified");
-                
+
                 session.update(user);
                 session.beginTransaction().commit();
-                
+
                 UserDTO userDTO = new UserDTO();
                 userDTO.setId(user.getId());
                 userDTO.setF_name(user.getF_name());
@@ -57,15 +133,16 @@ public class UserVerification extends HttpServlet {
                 userDTO.setEmail(email);
                 req.getSession().removeAttribute("email");
                 req.getSession().setAttribute("user", userDTO);
-                
+
                 responseDTO.setOk(true);
+                responseDTO.setMsg("Your account successfully verified!");
             } else {
                 responseDTO.setMsg("Invalid verification code");
             }
         } else {
             responseDTO.setMsg("Verification unavailable please sign in");
         }
-        
+
         resp.setContentType("application/json");
         resp.getWriter().write(gson.toJson(responseDTO));
     }
